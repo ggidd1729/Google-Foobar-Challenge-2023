6e861d73d3ece4dd26d695dde1f7b418d3970dc1
gengidd1729 <genthehungrycaterpillar@gmail.com>
2024-01-19 11:43:24 +1100

    fixed the aggregate entrance and exit functions

--- Level 4/Escape Pods/escape_pods.py
+++ Level 4/Escape Pods/escape_pods.py
@@ -1,46 +1,48 @@
 def solution(entrances, exits, path):
-    super_entrance = aggregate_entrances(entrances, path)
-    super_exit = aggregate_exits(exits, path)
+    append_super_entrance(entrances, path)
+    append_super_exit(exits, path)
 
-    # print("super entrance: ", super_entrance)
-    # print("super exit: ", super_exit)
+    print(path)
 
-    path.append(super_entrance)
-    path.append(super_exit)
-
-    return ford_fulkerson(path, -2, -1)
+    return ford_fulkerson(path, -2, -1, entrances)
 
 # creates a virtual "super entrance" to simplify the multi-source, multi-sink max flow problem to single-
-# -source, single-sink max flow
-def aggregate_entrances(entrances, path):
-    aggregated = path[entrances[0]]
+# source, single-sink max flow, appends it to path and adjust the path nodes (rooms)
+def append_super_entrance(entrances, path):
+    num_rooms = len(path[0])
+    super_entrance = [0] * num_rooms
 
-    for entrance in range (1,len(entrances)):
-        aggregated = [sum(i) for i in zip(aggregated, path[entrances[entrance]])]
+    for entrance in range(len(entrances)):
+        for next in path[entrance]:
+            super_entrance[entrance] += next
 
-    return aggregated
+    path.append(super_entrance)
+
+    return
 
 # creates a virtual "super exit" to simplify the multi-source, multi-sink max flow problem to single-
-# -source, single-sink max flow
-def aggregate_exits(exits, path):
+# source, single-sink max flow, appends it to path and adjust the path nodes (rooms)
+def append_super_exit(exits, path):
     num_rooms = len(path[0])
-    aggregated = [0] * num_rooms
+    super_exit = [0] * num_rooms
+    path.append(super_exit)
 
     for room in path:
+        room += 2 * [0]
         for exit in exits:
             if room[exit] != 0:
-                aggregated = [sum(i) for i in zip(aggregated, room)]
-                break
+                room[-1] += room[exit]
+                room[exit] = 0
 
-    return aggregated
+    return
 
 # breadth-first-search to find an augmenting from source to sink
-def bfs(path, source, sink, parent):
+def bfs(path, source, sink, parent, entrances):
     num_rooms = len(path)
     visited = [False] * num_rooms
     queue = []
     queue.append(source)
-    visited[source] = True
+    initialise_visited(visited, source, entrances)
 
     while queue:
         current = queue.pop(0)
@@ -49,16 +51,28 @@ def bfs(path, source, sink, parent):
                 queue.append(next)
                 visited[next] = True
                 parent[next] = current
-
+    #print(visited)
     if visited[sink]:
         return True
     return False
 
 #
-def ford_fulkerson(path, entrance, exit):
+def ford_fulkerson(path, entrance, exit, entrances):
+    parent = [-1]* len(path)
     max_flow = 0
 
+    bfs(path, entrance, exit, parent, entrances)
+
+    #print(parent)
+
     return max_flow
+
+#
+def initialise_visited(visited, source, entrances):
+    visited[source] = True
+    for entrance in entrances:
+        visited[entrance] = True
+
 
 
 # solution([0], [3], [[0, 7, 0, 0], [0, 0, 6, 0], [0, 0, 0, 8], [9, 0, 0, 0]]) 
