a11fb31411b023b6eae64cc58e41b9cb7eec5019
gengidd1729 <genthehungrycaterpillar@gmail.com>
2024-01-29 23:14:53 +1100

    fix readme, comment some initial ideas in the solution file

--- Level 4/Free the Bunny Workers/free_the_bunny_workers.py
+++ Level 4/Free the Bunny Workers/free_the_bunny_workers.py
@@ -1,3 +1,22 @@
 def solution(num_buns, num_required):
-    # Your code here
-    return
+    """
+    if num_required < num_buns:
+        return None
+    
+    elif num_required == num_buns:
+        bun_list = []
+        for key in range(num_required):
+            bun_list.append([key])
+        return bun_list
+    
+    elif num_required == 0:
+        bun_list = []
+        for bun in num_buns:
+            bun_list.append([])
+        return bun_list
+    
+    else:
+    """
+
+    
+    return None
