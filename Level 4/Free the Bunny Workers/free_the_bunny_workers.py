ee43da8f0d9924bbd8fdc866982b46650a2fce4a
gengidd1729 <genthehungrycaterpillar@gmail.com>
2024-02-02 14:51:43 +1100

    use a combinations approach (still need to reduce/select the correct lists)

--- Level 4/Free the Bunny Workers/free_the_bunny_workers.py
+++ Level 4/Free the Bunny Workers/free_the_bunny_workers.py
@@ -1,22 +1,53 @@
+from itertools import combinations
+
 def solution(num_buns, num_required):
-    """
-    if num_required < num_buns:
-        return None
     
-    elif num_required == num_buns:
+    # no bunnies needed..?
+    if num_required == 0:
         bun_list = []
-        for key in range(num_required):
-            bun_list.append([key])
-        return bun_list
-    
-    elif num_required == 0:
-        bun_list = []
-        for bun in num_buns:
+        for bun in range(num_buns):
             bun_list.append([])
         return bun_list
     
-    else:
-    """
-
+    # give the same key to each bunny
+    elif num_required == 1:
+        bun_list = []
+        for bun in range(num_buns):
+            bun_list.append([0])
+        return bun_list
     
+    # we ran out of bunnies!
+    elif num_required > num_buns:
+        return None
+
+    elif 1 < num_required <= num_buns:
+        bun_list = []
+        num_keys = choose(num_buns, num_required - 1)
+        num_per_key = num_buns - num_required + 1
+        total_num_keys = num_keys * num_per_key
+        keys_per_bun = total_num_keys // num_buns
+
+        """
+        print(num_keys)
+        print(num_per_key)
+        print(total_num_keys)
+        print(keys_per_bun)
+        """
+
+        combi = combinations(range(num_keys), keys_per_bun)
+        for i in combi:
+            bun_list.append(list(i))
+        return bun_list
+    
+    # invalid input
     return None
+
+# calculates the factorial of an integer
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    return (n * factorial(n-1))
+
+# calculates nCr
+def choose(n, r):
+    return factorial(n) // (factorial(r) * factorial(n - r))
