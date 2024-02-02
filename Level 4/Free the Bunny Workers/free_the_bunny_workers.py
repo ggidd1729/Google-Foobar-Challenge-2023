a6b5732b0be8d0d9c1efb178ba7e66357fd49ca8
gengidd1729 <genthehungrycaterpillar@gmail.com>
2024-02-02 17:20:06 +1100

    finish challenge with simpler approach

--- Level 4/Free the Bunny Workers/free_the_bunny_workers.py
+++ Level 4/Free the Bunny Workers/free_the_bunny_workers.py
@@ -1,53 +1,27 @@
 from itertools import combinations
 
 def solution(num_buns, num_required):
-    
-    # no bunnies needed..?
-    if num_required == 0:
-        bun_list = []
-        for bun in range(num_buns):
-            bun_list.append([])
-        return bun_list
-    
-    # give the same key to each bunny
-    elif num_required == 1:
-        bun_list = []
-        for bun in range(num_buns):
-            bun_list.append([0])
-        return bun_list
-    
-    # we ran out of bunnies!
-    elif num_required > num_buns:
-        return None
 
-    elif 1 < num_required <= num_buns:
-        bun_list = []
-        num_keys = choose(num_buns, num_required - 1)
-        num_per_key = num_buns - num_required + 1
-        total_num_keys = num_keys * num_per_key
-        keys_per_bun = total_num_keys // num_buns
-
-        """
-        print(num_keys)
-        print(num_per_key)
-        print(total_num_keys)
-        print(keys_per_bun)
-        """
-
-        combi = combinations(range(num_keys), keys_per_bun)
-        for i in combi:
-            bun_list.append(list(i))
-        return bun_list
-    
     # invalid input
+    if not (isinstance(num_buns, int) and isinstance(num_required, int)):
+        return None
+    
+    # num_buns should we between 1 and 9
+    if num_buns < 1 or num_buns > 9:
+        return None
+    
+    # num_required can't be negative or more than num_buns
+    if 0 <= num_required <= num_buns:
+        bun_key_list = [[] for bun in range(num_buns)]
+        num_keys_per_key = num_buns - (num_required - 1)
+
+        current_key = 0
+        for buns_for_current_key in combinations(range(num_buns), num_keys_per_key):
+            for bun in buns_for_current_key:
+                bun_key_list[bun].append(current_key)
+            current_key += 1
+
+        return bun_key_list
+    
+    # num_required out of range
     return None
-
-# calculates the factorial of an integer
-def factorial(n):
-    if n == 1 or n == 0:
-        return 1
-    return (n * factorial(n-1))
-
-# calculates nCr
-def choose(n, r):
-    return factorial(n) // (factorial(r) * factorial(n - r))
